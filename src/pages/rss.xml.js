1d4a3f7eb8506c1acb9ddfe1892cd8089abf3f66
DavidTchintcharauli <chincharaulidavid@gmail.com>
2024-04-30 00:04:19 +0400

    fix bug inside rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,11 @@
-import rss, { pagesGlobToRssItem } from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
-    return res({
-        title: 'Astro Learner | Blog',
-        description: 'My journey learning Astro',
-        site: context.site,
-        items: await pagesGlobToRssItem(import.meta.globe('./**/*.md')),
-        customData: `<language>en-us</language`,
-    })
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
 }
